4087d499c9fb61416a2fbc6a248672d08e2c6855
Jose Cano Jaramillo <jose.canojl@uanl.edu.mx>
2025-11-14 06:33:22 +0000

    stage

--- models/business_vault/pit_order_latest.sql
+++
@@ -1,8 +0,0 @@
-{{ config(schema='BUSINESS_VAULT', materialized='view') }}
-
--- Simple PIT example: last load per order (for demo)
-SELECT
-  o.hk_order,
-  MAX(o.load_ts) AS last_load_ts
-FROM {{ ref('sat_order_facts') }} o
-GROUP BY 1;
